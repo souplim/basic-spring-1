9231947320bdf2acb09d750f396b9300f99bb16c
souplim <ej0514@knou.ac.kr>
2024-03-13 20:44:51 +0900

    spring basic 1,2

---
+++ src/main/java/hello/core/member/MemoryMemberRepository.java
@@ -0,0 +1,17 @@
+package hello.core.member;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryMemberRepository implements MemberRepository {
+    private static Map<Long, Member> store = new HashMap<>(); // 여러 군데에서 접근하면 동시성 이슈가 있을 수 있기 때문에 concurrentHashMap을 사용해야 함
+    @Override
+    public void save(Member member) {
+        store.put(member.getId(), member);
+    }
+
+    @Override
+    public Member findById(Long memberId) {
+        return store.get(memberId);
+    }
+}
